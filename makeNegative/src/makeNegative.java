4b9f4e1c0b9e77e66a62037be0ce6556e20a9eba
Brett Gamble <brettkgamble@gmail.com>
2019-12-17 11:02:19 -0700

    0003 makeNegative.Java / makeNegative.java

---
+++ makeNegative/src/makeNegative.java
@@ -0,0 +1,14 @@
+public class makeNegative {
+
+
+    public static int main(String[] args) {
+        int n = -1;
+
+        if (n <= 0) {
+            return n;
+        } else {
+            return n * -1;
+        }
+    }
+
+}
