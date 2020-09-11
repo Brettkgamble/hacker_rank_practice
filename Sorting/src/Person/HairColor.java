1d2f9b992fcbbb2b4f52fde4a81d059f0d93bbe2
brettkgamble@gmail.com <5413CrossFit2018>
2020-09-11 15:10:38 -0600

    000001 - Added new person class under Sorting.  Also added hairColor Enum

--- Sorting/src/Person/HairColor.java
+++ Sorting/src/Person/HairColor.java
@@ -1,5 +1,5 @@
 package Person;
 
 public enum HairColor {
-
+    BLONDE, BROWN, BLACK, RED, ORANGE, PINK, PURPLE, BLUE, GREEN, RAINBOW, OTHER
 }
--- Sorting/src/Person/Person.java
+++ Sorting/src/Person/Person.java
@@ -1,9 +1,22 @@
 package Person;
 
+import static Person.HairColor.*;
+
+
 public class Person {
-    String hairColor;
+    HairColor hairColor = BLACK;
+
+    public Person() {
+
+    }
 
     public static void main(String[] args) {
+        Person peterParker = new Person();
+        Person spiderman = peterParker;
 
+        spiderman.hairColor = PINK;
+
+        System.out.println("Hair Color of Peter PArker " + peterParker.hairColor);
+        System.out.println("Hair Color of Spiderman " + spiderman.hairColor);
     }
 }
